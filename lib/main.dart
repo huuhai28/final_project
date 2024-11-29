fa168f98dbc9258673ef51dabad405287881dccf
Hai <nguyenhai281103@gmail.com>
2024-11-30 00:47:57 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'view/login_screen.dart';
+import 'view/main_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const AppMainScreen();
+          } else {
+            return const LoginScreen();
+          }
+        },
+      ),
+    );
+  }
+}
